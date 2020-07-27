90d30e347bf39e09f14a1d66f75b6f6b3853bfcb
petiteoh <nikkiteoh@gmail.com>
2020-07-27 17:09:34 -0400

    prepare for initial heroku push

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./frontend/bench_bnb.jsx",
+  entry: "./frontend/nommly.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js",
